35d97a90ddc8a0869bf431c58fc2eb41cd3e9037
Anish Patel <anish.patel.1829405.ap@gmail.com>
2022-07-10 16:01:43 +0530

    Added Photo In HomePage

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
 import './HomePage.css'
+import Picture from './picture.jpg'
 
 const HomePage = () => {
     return (
         <div className='homepage-maincontainer'>
-            <div className='homepage-welcometext'>
-                <h1>Welcome</h1>
+            <div className='homepage-textcontainer'>
+                <div className='homepage-welcometext'>
+                    <h1>Welcome</h1>
+                </div>
+                <div className='homepage-besttext'>
+                    <h3>Have Your Best Chat</h3>
+                </div>
             </div>
-            <div className='homepage-besttext'>
-                <h3>Have Your Best Chat</h3>
+            <div className='homepage-imagecontainer'>
+                <img src={Picture} />
             </div>
         </div>
     )
